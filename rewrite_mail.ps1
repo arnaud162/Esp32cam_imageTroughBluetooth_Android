1712ada6316e9d6d681a0a01850deb087fb1735b
arnaud162 <arnaud162@users.noreply.github.com>
2025-03-10 00:10:32 +0100

    removed email modifying script

--- rewrite_mail.ps1
+++
@@ -1,7 +0,0 @@
-git filter-repo --email-callback "
-print(email)
-if email == b'arnaud.dejarcy@gmail.com':
-    #print('va rempl')
-    return b'arnaud162@users.noreply.github.com'
-    
-return email" --force
